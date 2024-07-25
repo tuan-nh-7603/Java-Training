55cf2451df64c50e806d7e88dbf1d15bbc60056b
HoangTranHust <hoangtrandtyt17@gmail.com>
2024-07-26 02:11:27 +0700

    Add COntrol Statement example

---
+++ JavaCore/ControlStatement/src/ControlStatementExample.java
@@ -0,0 +1,87 @@
+public class ControlStatementExample {
+    public static void main(String[] args) {
+        //if else
+//        int i = 5;
+//        if (time < 10) {
+//            System.out.println("Java");
+//        } else if (time < 15) {
+//            System.out.println("Python");
+//        } else {
+//            System.out.println("PHP");
+//        }
+
+        // switch case
+//        int day = 3;
+//        switch (day) {
+//            case 2:
+//                System.out.println( "Monday"); ;
+//                break;
+//            case 3:
+//                System.out.println("Tuesday") ;
+//                break;
+//            case 4:
+//                System.out.println( "Wednesday") ;
+//                break;
+//            case 5:
+//                System.out.println( "Thursday") ;
+//                break;
+//            default:
+//                System.out.println( "Invalid ") ;
+
+        //for loop
+//        for (int i = 0; i < 20; i++) {
+//            System.out.println(i);
+//        }
+
+        //while loop
+//        int i = 1;
+//        while (i <= 10) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //do-while loop
+//        int i = 1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while(i <= 10);
+
+
+        //Nested loop
+//        for (int i = 1; i <= 3; i++) {
+//            System.out.println("Outer: " + i);
+//            // Inner loop
+//            for (int j = 1; j <= 4; j++) {
+//                System.out.println(" Inner: " + j);
+//            }
+//        }
+
+        //break
+//        for (int i = 0; i < 7; i++) {
+//            if (i == 3) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+        //continue
+//        for (int i = 0; i < 7; i++) {
+//            if (i == 3) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        //break with label
+//        outer:
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.println("i = " + i + ", j = " + j);
+//                if (i == 1 && j == 2) {
+//                    break outer;
+//                }
+//            }
+//        }
+
+    }
+}
